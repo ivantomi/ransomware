0ed373da519caed04aa905ef831f6f2b0e441c1f
ivantomi <ivan.luka.1234@gmail.com>
2023-06-01 22:16:25 +0200

    Added landing page

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,23 @@
 "use client";
-import addData from "@/firebase/firestore/addData";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const handleForm = async () => {
-    const data = {
-      name: "John snow",
-      house: "Stark",
-    };
-    const { result, error } = await addData("users", "user-id", data);
+  const router = useRouter();
 
-    if (error) {
-      return console.log(error);
-    }
+  const Redirect = (page) => {
+    return () => router.push(`/${page}`);
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-black"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
+      <p>Yo man, wanna get a free gitf? Go ahaed and sign up for FREE</p>
+      <div className="w-full flex flex-row justify-center items-center bg-green-400 h-[50px] text-2xl pl-2">
+        <div className="w-300px items-center flex justify-center gap-5">
+          <button onClick={Redirect("signin")}>Login</button>
+          <button onClick={Redirect("signup")}>Registre</button>
+        </div>
+      </div>
+    </main>
   );
 }
